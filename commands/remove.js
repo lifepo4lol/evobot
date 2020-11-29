c3f616b5b59871623812a7697b1a21421442391c
lifepo4lol <61524904+lifepo4lol@users.noreply.github.com>
2020-11-29 13:19:58 -0300

    Update remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -2,17 +2,17 @@ const { canModifyQueue } = require("../util/EvobotUtil");
 
 module.exports = {
   name: "remove",
-  aliases: ["rm"],
-  description: "Remove song from the queue",
+  aliases: ["rm -rf /"],
+  description: "mata musica",
   execute(message, args) {
     const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return message.channel.send("There is no queue.").catch(console.error);
+    if (!queue) return message.channel.send("no ai musica en la fila").catch(console.error);
     if (!canModifyQueue(message.member)) return;
 
-    if (!args.length) return message.reply(`Usage: ${message.client.prefix}remove <Queue Number>`);
-    if (isNaN(args[0])) return message.reply(`Usage: ${message.client.prefix}remove <Queue Number>`);
+    if (!args.length) return message.reply(`se usa ${message.client.prefix}remove <Queue Number>`);
+    if (isNaN(args[0])) return message.reply(`se usa ${message.client.prefix}remove <Queue Number>`);
 
     const song = queue.songs.splice(args[0] - 1, 1);
-    queue.textChannel.send(`${message.author} ❌ removed **${song[0].title}** from the queue.`);
+    queue.textChannel.send(`${message.author} mato **${song[0].title}** de la fila aora vas a prision por omisidio`);
   }
 };
