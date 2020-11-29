8cac7f91a75c7da5e4b1ab3f4277342a0689fe20
lifepo4lol <61524904+lifepo4lol@users.noreply.github.com>
2020-11-29 13:23:00 -0300

    Update resume.js

--- commands/resume.js
+++ commands/resume.js
@@ -3,18 +3,18 @@ const { canModifyQueue } = require("../util/EvobotUtil");
 module.exports = {
   name: "resume",
   aliases: ["r"],
-  description: "Resume currently playing music",
+  description: "despausiona la musicasion",
   execute(message) {
     const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return message.reply("There is nothing playing.").catch(console.error);
+    if (!queue) return message.reply("no aber nada reprodusiendo ekisde").catch(console.error);
     if (!canModifyQueue(message.member)) return;
 
     if (!queue.playing) {
       queue.playing = true;
       queue.connection.dispatcher.resume();
-      return queue.textChannel.send(`${message.author} ▶ resumed the music!`).catch(console.error);
+      return queue.textChannel.send(`${message.author} despauso la musicasion`).catch(console.error);
     }
 
-    return message.reply("The queue is not paused.").catch(console.error);
+    return message.reply("no esta pausado pendejo").catch(console.error);
   }
 };
