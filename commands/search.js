21cc72d187a539d2ec707c5404622e6902e5a2a2
lifepo4lol <61524904+lifepo4lol@users.noreply.github.com>
2020-11-29 13:25:39 -0300

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -5,22 +5,22 @@ const youtube = new YouTubeAPI(YOUTUBE_API_KEY);
 
 module.exports = {
   name: "search",
-  description: "Search and select videos to play",
+  description: "vusca bideo i musicasion",
   async execute(message, args) {
     if (!args.length)
       return message
         .reply(`Usage: ${message.client.prefix}${module.exports.name} <Video Name>`)
         .catch(console.error);
     if (message.channel.activeCollector)
-      return message.reply("A message collector is already active in this channel.");
+      return message.reply("i no c que dise algo paso no c");
     if (!message.member.voice.channel)
-      return message.reply("You need to join a voice channel first!").catch(console.error);
+      return message.reply("entra en un canal de bos primero pendejo").catch(console.error);
 
     const search = args.join(" ");
 
     let resultsEmbed = new MessageEmbed()
-      .setTitle(`**Reply with the song number you want to play**`)
-      .setDescription(`Results for: ${search}`)
+      .setTitle(`**responde con el nomvre de la musica que kere**`)
+      .setDescription(`resultado para ${search}`)
       .setColor("#F8AA2A");
 
     try {
