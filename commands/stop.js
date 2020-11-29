5edca8cb47b501cc7846d35988f5bb2640939043
lifepo4lol <61524904+lifepo4lol@users.noreply.github.com>
2020-11-29 13:30:00 -0300

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -2,15 +2,15 @@ const { canModifyQueue } = require("../util/EvobotUtil");
 
 module.exports = {
   name: "stop",
-  description: "Stops the music",
+  description: "para la musicasion",
   execute(message) {
     const queue = message.client.queue.get(message.guild.id);
 
-    if (!queue) return message.reply("There is nothing playing.").catch(console.error);
+    if (!queue) return message.reply("no ai nada reprodusiendo pendejo").catch(console.error);
     if (!canModifyQueue(message.member)) return;
 
     queue.songs = [];
     queue.connection.dispatcher.end();
-    queue.textChannel.send(`${message.author} ⏹ stopped the music!`).catch(console.error);
+    queue.textChannel.send(`${message.author} paro la musica`).catch(console.error);
   }
 };
