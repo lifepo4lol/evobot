06923e557edb8a90b7f6db5dfdacb2b7eb482c26
lifepo4lol <61524904+lifepo4lol@users.noreply.github.com>
2020-11-29 13:03:32 -0300

    Update pause.js

--- commands/pause.js
+++ commands/pause.js
@@ -2,16 +2,16 @@ const { canModifyQueue } = require("../util/EvobotUtil");
 
 module.exports = {
   name: "pause",
-  description: "Pause the currently playing music",
+  description: "ase la pausasion",
   execute(message) {
     const queue = message.client.queue.get(message.guild.id);
-    if (!queue) return message.reply("There is nothing playing.").catch(console.error);
+    if (!queue) return message.reply("no hai nada reprodusiendo subnormal de mierda").catch(console.error);
     if (!canModifyQueue(message.member)) return;
 
     if (queue.playing) {
       queue.playing = false;
       queue.connection.dispatcher.pause(true);
-      return queue.textChannel.send(`${message.author} ⏸ paused the music.`).catch(console.error);
+      return queue.textChannel.send(`${message.author} ese tipo pauso la musica`).catch(console.error);
     }
   }
 };
