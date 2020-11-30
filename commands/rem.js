c8960c2805da6c577146c809991f2c5a4907b535
lifepo4lol <61524904+lifepo4lol@users.noreply.github.com>
2020-11-30 00:54:21 -0300

    Update rem.js

--- commands/rem.js
+++ commands/rem.js
@@ -1,11 +1,4 @@
-const Booru = require('./dist')
-const { BooruError, sites } = require('./dist')
-// for ES6:
-// import Booru, { search, BooruError, sites } from 'booru'
-
-const argTags = process.argv.slice(3)
-const site = process.argv[2] || 'sb'
-const tags = process.argv[2] ? argTags : ['cat']
+const Booru = require('booru')
 
 // Search with promises
 module.exports = {
@@ -13,23 +6,8 @@ module.exports = {
   aliases: ["rem34"],
   description: "estoi probando ok?",
 }
-Booru.search(site, tags, { limit: 1, random: true })
+Booru.search('gelbooru', ['rem_(re:zero)'], { limit: 3, random: true })
   .then(posts => {
-    if (posts.length === 0) {
-      console.log('No images found.')
-    }
-
-    for (let post of posts) {
-      console.log(post.fileUrl)
-    }
-  })
-  .catch(err => {
-    if (err instanceof BooruError) {
-      // It's a custom error thrown by the package
-      // Typically results from errors the boorus returns, eg. "too many tags"
-      console.error(err)
-    } else {
-      // This means something pretty bad happened
-      console.error(err)
-    }
+    for (let post of posts)
+      console.log(post.fileUrl, post.postView)
   })
