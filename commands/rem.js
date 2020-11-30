45a9721297f1344ed1e68193cc0014114c7e136b
lifepo4lol <61524904+lifepo4lol@users.noreply.github.com>
2020-11-30 12:59:10 -0300

    Update rem.js

--- commands/rem.js
+++ commands/rem.js
@@ -1,18 +1,18 @@
-const Booru = require('./dist')
-const { BooruError, sites } = require('./dist')
+//const Booru = require('./dist')
+//const { BooruError, sites } = require('./dist')
 
-module.exports = {
+//module.exports = {
   //name: "rem",
- aliases: ["rem34"],
-description: "estoi probando ok?",
-}
-Booru.search(site, tags, { limit: 1, random: false })
-  .then(posts => {
-    if (posts.length === 0) {
-      console.log('No images found.')
-    }
+// aliases: ["rem34"],
+//description: "estoi probando ok?",
+//}
+//Booru.search(site, tags, { limit: 1, random: false })
+//  .then(posts => {
+//   if (posts.length === 0) {
+//      console.log('No images found.')
+//    }
 
-    for (let post of posts) {
-      console.log(post.fileUrl)
-    }
-  })
+//    for (let post of posts) {
+//     console.log(post.fileUrl)
+//    }
+//  })
