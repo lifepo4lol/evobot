42a816e5189d2bf397058d15eb7a819895d4c720
lifepo4lol <61524904+lifepo4lol@users.noreply.github.com>
2020-11-30 12:29:30 -0300

    Update rem.js

--- commands/rem.js
+++ commands/rem.js
@@ -1,12 +1,13 @@
-const Booru = require('booru')
+import Booru, { search, BooruError, sites } from 'booru'
 
 module.exports = {
   //name: "rem",
  aliases: ["rem34"],
 description: "estoi probando ok?",
-}
+
 async function booruSearch(site, tags, limit = 1, random = true) {
   const posts = await Booru.search(site, tags, {limit, random})
 
   return console.log(posts[0].fileUrl)
+}
 }
