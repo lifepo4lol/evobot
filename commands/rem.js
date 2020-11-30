056cb229c084e2c34027eb1e3418eab328d68b31
lifepo4lol <61524904+lifepo4lol@users.noreply.github.com>
2020-11-30 00:33:33 -0300

    Update rem.js

--- commands/rem.js
+++ commands/rem.js
@@ -2,17 +2,23 @@ const { MessageEmbed } = require("discord.js");
 const Booru = require('booru');
 const Booru = require('./dist')
 const { BooruError, sites } = require('./dist')
+const argTags = process.argv.slice(3)
+const site = process.argv[2] || 'sb'
+const tags = process.argv[2] ? argTags : ['rem_(re:zero)']'
 
 module.exports = {
   name: "rem",
   aliases: ["rem34"],
   description: "no funsionar por aora pero despues funsionar xdd",
   //ase la vuscasion
-  Booru.search('gelbooru', ['rem_(re:zero) '], { limit: 3, random: true })
+Booru.search(site, tags, { limit: 1, random: false })
   .then(posts => {
-    for (let post of posts)
-      console.log(post.fileUrl, post.postView)
-      return message.reply("aca tene xdd **${post.fileUrl}** ").catch(console.error);
-  })
+    if (posts.length === 0) {
+      console.log('No images found.')
+    }
 
+    for (let post of posts) {
+      console.log(post.fileUrl)
+    }
+  })
 };
