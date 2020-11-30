ddd209447080b823972bf2a1628ae65a0a3e6c50
lifepo4lol <61524904+lifepo4lol@users.noreply.github.com>
2020-11-30 00:40:48 -0300

    Update rem.js

--- commands/rem.js
+++ commands/rem.js
@@ -6,11 +6,7 @@ const { BooruError, sites } = require('./dist')
 const argTags = process.argv.slice(3)
 const site = process.argv[2] || 'sb'
 const tags = process.argv[2] ? argTags : ['cat']
-module.exports = {
-  name: "rem",
-  aliases: ["rem34"],
-  description: "no funsionar por aora pero despues funsionar xdd",
-  //ase la vuscasion
+
 // Search with promises
 Booru.search(site, tags, { limit: 1, random: false })
   .then(posts => {
@@ -22,4 +18,36 @@ Booru.search(site, tags, { limit: 1, random: false })
       console.log(post.fileUrl)
     }
   })
-};
+  .catch(err => {
+    if (err instanceof BooruError) {
+      // It's a custom error thrown by the package
+      // Typically results from errors the boorus returns, eg. "too many tags"
+      console.error(err)
+    } else {
+      // This means something pretty bad happened
+      console.error(err)
+    }
+  })
+
+// Search with async/await
+async function booruSearch(site, tags, limit = 1, random = true) {
+  const posts = await Booru.search(site, tags, {limit, random})
+
+  return console.log(posts[0].fileUrl)
+}
+
+// Create an instance of a booru to use yourself
+// This allows you to create a booru with certain credentials/settings and reuse it
+// Internally, `Booru.search` just creates boorus and caches them
+// Ex: `Booru.forSite('safebooru')`
+async function booruClassSearch(site, tags, limit = 1, random = true) {
+  const myBooru = Booru.forSite(site)
+  const posts = await myBooru.search(tags, {limit, random})
+
+  return console.log(posts[0].fileUrl)
+}
+
+// You can also check the sites and the options for each
+//   console.log(Booru.sites)
+// Or just the site URLs
+//   console.log(Object.keys(sites))
